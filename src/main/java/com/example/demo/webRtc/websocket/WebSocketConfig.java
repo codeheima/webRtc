16f8349044d6f143624023c624cdf56eebf50e72
jianhua.ning <ningjh@onecloud.cn>
2018-11-04 20:57:04 +0800

    update

--- src/main/java/com/example/demo/webRtc/websocket/WebSocketConfig.java
+++ src/main/java/com/example/demo/webRtc/websocket/WebSocketConfig.java
@@ -1,14 +1,22 @@
 package com.example.demo.webRtc.websocket;
 
+import org.kurento.client.KurentoClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
+	
+	private static final String DEFAULT_KMS="ws://192.168.33.134:8888/kurento";
 	@Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+
+	@Bean
+	public KurentoClient kurentoClient() {
+		return KurentoClient.create(DEFAULT_KMS);
+	}
 
 }
